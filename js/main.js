0347ef2562087d6812a0e6f35b5741e3b1b313c9
GriffithDidNothingWrong <fabricio.tabbia@gmail.com>
2023-10-04 19:53:16 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -7,14 +7,10 @@ class Viaje {
         this.descripcion = descripcion;
         this.distancia = parseFloat(distancia);
         this.tripulantes = parseInt(tripulantes);
-    };
-    presupuestoViaje (){
-        return this.distancia * 1500 * this.tripulantes;
-    }
-    duracionViaje(){
         let tiempoViaje = this.distancia/velocidadNave;
-        return parseInt(tiempoViaje*60) + " MINUTOS------equivalente a: " + parseInt(tiempoViaje) + " HORAS------equivalente a: " + parseInt(tiempoViaje/24) + " DIAS";
-    }
+        this.presupuesto = "El presupuesto es: " + this.distancia * 1500 * this.tripulantes;
+        this.duracion = parseInt(tiempoViaje*60) + " MINUTOS------equivalente a: " + parseInt(tiempoViaje) + " HORAS------equivalente a: " + parseInt(tiempoViaje/24) + " DIAS";
+    };
 };
 
 let arrViajes = [];
@@ -30,73 +26,60 @@ while (check != "listo" || check != "Listo" || check != "LISTO") {
         let checkDistancia = parseFloat(prompt("Distancia desde la tierra del sistema solar en años luz (Minimo 4.3)", "4.3"));
         console.log(checkDistancia);
         if (checkDistancia < 4.3 || isNaN(checkDistancia)){
-            alert("Invalid")
+            alert("Invalid");
             continue;
         };
-        let cantidadTripulantes = parseInt(prompt("Cantidad de tripulantes (Minimo 1)"))
+        let cantidadTripulantes = parseInt(prompt("Cantidad de tripulantes (Minimo 1)", "1"));
         console.log(cantidadTripulantes);
         if (cantidadTripulantes <= 0 || isNaN(cantidadTripulantes)){
-            alert("invalid")
+            alert("invalid");
             continue;
         }
-        arrViajes.push(new Viaje(planetaNombre,planetaTipo,planetaDescripcion,checkDistancia,cantidadTripulantes))         
+        arrViajes.push(new Viaje(planetaNombre,planetaTipo,planetaDescripcion,checkDistancia,cantidadTripulantes));         
     }   
 }
 console.log(arrViajes);
 
 for (let viaje of arrViajes){
-    document.write("<ul><li><h3>Nombre del planeta: " + viaje.planeta + "</h3></li>");
-    document.write("<li><h3>Tipo de planeta: " + viaje.tipo + "</h3></li>");
-    document.write("<li><h3>Descripcion del planeta: " + viaje.descripcion + "</h3></li>");
-    document.write("<li><h3>Distancia respecto a la tierra: " + viaje.distancia + "</h3></li>");
-    document.write("<li><h3>Cantidad de tripulantes: " + viaje.tripulantes + "</h3></li>");
-    document.write("<li><h3>Coste del viaje: " + viaje.presupuestoViaje() + "</h3></li>");
-    document.write("<li><h3>Duracion del viaje: " + viaje.duracionViaje() + "</h3></li></ul></br>");
+    console.log(viaje);
 }
 
 let viajesCortos = arrViajes.filter(Viaje => Viaje.distancia<= 15456);
-document.write("<h2> Viajes de corta duracion : </h2>")
+alert("viaje de corta duracion");
 
 for (let viaje of viajesCortos){
-    document.write("<ul><li><h3>Nombre del planeta: " + viaje.planeta + "</h3></li>");
-    document.write("<li><h3>Duracion del viaje: " + viaje.duracionViaje() + "</h3></li>");
-    document.write("<li><h3>Tipo de planeta: " + viaje.tipo + "</h3></li>");
-    document.write("<li><h3>Descripcion del planeta: " + viaje.descripcion + "</h3></li>");
-    document.write("<li><h3>Distancia respecto a la tierra: " + viaje.distancia + "</h3></li></ul></br>");
+    alert(viaje.planeta)
+    console.log("Viajes de corta duracion");
+    console.log(viaje);
 }
 
 let viajesMedios = arrViajes.filter(Viaje => Viaje.distancia > 15456 && Viaje.distancia <= 92736);
-document.write("<h2> Viajes de media duracion : </h2>")
+alert("viajes de media duracion");
 
 for (let viaje of viajesMedios){
-    document.write("<ul><li><h3>Nombre del planeta: " + viaje.planeta + "</h3></li>");
-    document.write("<li><h3>Duracion del viaje: " + viaje.duracionViaje() + "</h3></li>");
-    document.write("<li><h3>Tipo de planeta: " + viaje.tipo + "</h3></li>");
-    document.write("<li><h3>Descripcion del planeta: " + viaje.descripcion + "</h3></li>");
-    document.write("<li><h3>Distancia respecto a la tierra: " + viaje.distancia + "</h3></li></ul></br>");
+    alert(viaje.planeta)
+    console.log("Viajes de media duracion");
+    console.log(viaje);
 }
 
 let viajesLargos = arrViajes.filter(Viaje => Viaje.distancia > 92736);
-document.write("<h2> Viajes de larga duracion : </h2>")
+alert("viajes de larga duracion");
 
 for (let viaje of viajesLargos){
-    document.write("<ul><li><h3>Nombre del planeta: " + viaje.planeta + "</h3></li>");
-    document.write("<li><h3>Duracion del viaje: " + viaje.duracionViaje() + "</h3></li>");
-    document.write("<li><h3>Tipo de planeta: " + viaje.tipo + "</h3></li>");
-    document.write("<li><h3>Descripcion del planeta: " + viaje.descripcion + "</h3></li>");
-    document.write("<li><h3>Distancia respecto a la tierra: " + viaje.distancia + "</h3></li></ul></br>");
+    alert(viaje.planeta)
+    console.log("Viajes de larga duracion");
+    console.log(viaje);
 }
 
-let buscar = prompt("Ingresa el nombre del planeta que quieres buscar tu viaje")
-let buscarPlaneta = arrViajes.filter(Viaje => Viaje.planeta.includes(buscar))
+let buscar = prompt("Ingresa el nombre del planeta que quieres buscar tu viaje");
+let buscarPlaneta = arrViajes.filter(Viaje => Viaje.planeta.includes(buscar));
 
-document.write("<h2>Viajes encontrados por nombre de planetas</h2>")
+alert("Viajes encontrados con ese nombre");
 
 for(let viaje of buscarPlaneta){
-    document.write("<ul><li><h3>Nombre del planeta: " + viaje.planeta + "</h3></li>");
-    document.write("<li><h3>Duracion del viaje: " + viaje.duracionViaje() + "</h3></li>");
-    document.write("<li><h3>Cantidad de tripulantes: " + viaje.tripulantes + "</h3></li>");
-    document.write("<li><h3>Coste: " + viaje.presupuestoViaje() + "</h3></li></ul></br>");
+alert(viaje.planeta)
+console.log("Viajes encontrados con ese nombre");
+console.log(viaje);
 }
 
 
